1fe83628214086844160fe6b47de03e380174fd6
zhangji-h <zhangjl-h@glodon.com>
2018-12-17 15:25:52 +0800

    核心代码修改

--- xxl-job-executor-springboot/src/main/java/com/xxl/job/executor/service/overseer/StatusServiceImpl.java
+++ xxl-job-executor-springboot/src/main/java/com/xxl/job/executor/service/overseer/StatusServiceImpl.java
@@ -3,6 +3,10 @@ package com.xxl.job.executor.service.overseer;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.xxl.job.api.enums.JobStatus;
 import com.xxl.job.api.service.StatusService;
+import com.xxl.job.core.biz.model.HandleCallbackParam;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.thread.TriggerCallbackThread;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by dul-c on 2018-12-12.
@@ -13,6 +17,7 @@ import com.xxl.job.api.service.StatusService;
 		protocol = "${dubbo.protocol.id}",
 		registry = "${dubbo.registry.id}"
 )
+@Component
 public class StatusServiceImpl implements StatusService {
 	/**
 	 * Status Report 状态汇报
@@ -24,6 +29,8 @@ public class StatusServiceImpl implements StatusService {
 	 */
 	@Override
 	public Integer report(Integer taskInstanceId, String ip, Integer status) {
+		ReturnT result = new ReturnT(status, null);
+		TriggerCallbackThread.pushCallBack(new HandleCallbackParam(taskInstanceId, System.currentTimeMillis(), result));
 		return 0;
 	}
 
@@ -37,6 +44,8 @@ public class StatusServiceImpl implements StatusService {
 	 */
 	@Override
 	public Integer report(Integer taskInstanceId, String ip, JobStatus jobStatus) {
+		ReturnT result = new ReturnT(Integer.valueOf(jobStatus.getValue()), null);
+		TriggerCallbackThread.pushCallBack(new HandleCallbackParam(taskInstanceId, System.currentTimeMillis(), result));
 		return 0;
 	}
 }

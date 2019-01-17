6f4d9169a3c53da83ca3f790bc3d8b38f781dead
zhangji-h <zhangjl-h@glodon.com>
2019-01-17 13:07:10 +0800

    定时任务gojs展示flowchart

---
+++ xxl-job-api/src/main/java/com/xxl/job/api/service/XxlJobAdminJobInfoTriggerService.java
@@ -0,0 +1,29 @@
+package com.xxl.job.api.service;
+
+/**
+ * @desc 通过api触发任务
+ * Created by zhangjianlong on 2019/1/10.
+ *  @return 0：成功 1 :没有找到对应的任务 2：找到多个任务
+ */
+public interface XxlJobAdminJobInfoTriggerService {
+    /**
+     * 通过jobId触发任务
+     * @param jobId
+     * @return 0：成功
+     */
+    Integer triggerByJobId(Integer jobId);
+
+    /**
+     * 通过jobDesc触发任务
+     * @param jobDesc
+     * @return 0：成功
+     */
+    Integer triggerByJobDesc(String  jobDesc);
+
+    /**
+     * 通过mqKey触发任务
+     * @param mqKey
+     * @return 0：成功
+     */
+    Integer triggerByMqKey(String  mqKey);
+}
